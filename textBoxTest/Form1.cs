ad89df34d0d26e624c1ff13035e7865db378e699
wahidsharifi8 <135712175+wahidsharifi8@users.noreply.github.com>
2023-06-07 20:47:13 +0430

    Revert "Create avc_My Video-2.mp4"
    This reverts commit b8cc91bd1bc72509d51904947d4e60a31e466a83.

---
+++ textBoxTest/Form1.cs
@@ -0,0 +1,26 @@
+namespace textboxtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Completed");
+
+        }
+    }
+}
