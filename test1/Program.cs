dc9a28eace70e7278243d5f63b2df2784a966f31
wahidsharifi8 <135712175+wahidsharifi8@users.noreply.github.com>
2023-06-07 20:51:56 +0430

    program for built in fucntion

---
+++ test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is built in method ");
+            int var = 100;
+            Console.WriteLine(var);
+
+            int var1 = 64;
+            Console.WriteLine(Math.Sqrt(var1));
+
+            Console.ReadKey();
+
+        
+
+        }
+    }
+}
